ee795f908493a56279e90bde577a745bb7e993b1
Jorge López Puebla <jlpenero2005@gmail.com>
2024-05-30 10:27:09 +0200

    Commit

--- api/index.py
+++ api/index.py
@@ -1,7 +1,24 @@
+import os
+import requests
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+# DEFINE ENVIRONMENT VARIABLES
+GITHUB_KEY = os.environ.get('GITHUB_KEY')
+NOTION_KEY = os.environ.get('NOTION_KEY')
+
+@app.get("/api/syncHelpingHandsIssues")
+async def syncHelpingHandsIssues():
+    url = "https://api.github.com/repos/Jorge-lopz/Helping-Hands/issues"
+    authorization = "Bearer " + GITHUB_KEY
+    headers = {
+        "Accept": "application/vnd.github.text+json",
+        "Authorization": authorization
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response
+    else:
+        return None
